4e4f09424a2d06d927867f39a77e57bc61034c6a
azilzor <athirsonarceus@gmail.com>
2023-03-12 16:12:25 -0300

    initial commit

---
+++ src/test/kotlin/com/app/KotlinSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
